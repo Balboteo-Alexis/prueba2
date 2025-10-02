d88839e2f0fe91280d82aaf595fa39b194184aea
“Alexis <alexisbalboteo15@gmail.com>
2025-10-02 16:55:13 +0200

    .

--- CalculadoraBasica/CalculadoraBasica.java
+++ CalculadoraBasica/CalculadoraBasica.java
@@ -1,9 +1,30 @@
+/*
+ * 
+ * Autor: Alexis Balboteo
+ * Fecha: 02/10/2025
+ * 
+ */
+
+
+
 package CalculadoraBasica;
+import java.util.Scanner;
 
 public class CalculadoraBasica {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		
+		Scanner teclado = new Saneer(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
